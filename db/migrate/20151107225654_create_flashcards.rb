79d5d29dce8ec6136666d2b45bc9d1c1af0e13ba
Emily Dowdle <emily@editingemily.com>
2015-11-07 15:57:56 -0700

    Generate subject and flashcard models

---
+++ db/migrate/20151107225654_create_flashcards.rb
@@ -0,0 +1,10 @@
+class CreateFlashcards < ActiveRecord::Migration
+  def change
+    create_table :flashcards do |t|
+      t.text :question
+      t.text :answer
+
+      t.timestamps null: false
+    end
+  end
+end
